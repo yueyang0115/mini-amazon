acecd3071b009c83c1724baa818318b1857e6ab9
yy258 <yy258@duke.edu>
2020-04-17 01:36:47 -0400

    add item images in list_package

--- ERSSHW5/amazon/views.py
+++ ERSSHW5/amazon/views.py
@@ -122,8 +122,22 @@ def buy(request):
 
 @login_required
 def list_package(request):
+    package_list = Package.objects.filter(owner=request.user).order_by('creation_time')
+    item_dict = {}
+    price_dict = {}
+
+    for pack in package_list:
+        orders = Order.objects.filter(package__id=pack.id)
+        item_dict[pack.id] = orders
+        total = 0
+        for o in orders:
+            total += o.total()
+        price_dict[pack.id] = total
+
     context = {
-        'package_list': Package.objects.filter(owner=request.user).order_by('creation_time')
+        'package_list': package_list,
+        'item_dict':item_dict,
+        'price_dict':price_dict
     }
     return render(request, 'amazon/list_package.html', context)
 
