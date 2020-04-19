24479bfe5ca8b765c322ce6dab169cf21879a4d2
yueyang <yueyang0115@outlook.com>
2020-04-19 18:46:59 -0400

    add delete

--- ERSSHW5/amazon/views.py
+++ ERSSHW5/amazon/views.py
@@ -179,6 +179,28 @@ def list_package(request):
     }
     return render(request, 'amazon/list_package.html', context)
 
+@login_required
+def delete_package(request, package_id):
+    Package.objects.get(owner=request.user, id=package_id).delete()
+
+    #copy from list_package
+    package_list = Package.objects.filter(owner=request.user).order_by('creation_time')
+    item_dict = {}
+
+    if request.method == "POST":
+        search = request.POST["search"]
+        package_list = Package.objects.filter(owner=request.user, id=search)
+
+    for pack in package_list:
+        orders = Order.objects.filter(package__id=pack.id)
+        item_dict[pack.id] = orders
+
+    context = {
+        'package_list': package_list,
+        'item_dict': item_dict,
+    }
+
+    return render(request, 'amazon/list_package.html', context)
 
 @login_required
 def list_package_detail(request, package_id):
